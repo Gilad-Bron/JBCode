2c1e949b27b5b387338141adbf402f0c1f695a1f
Gilad <gilad.bron@gmail.com>
2024-05-06 19:22:47 +0300

    commit

--- JB_Project_01/script.js
+++ JB_Project_01/script.js
@@ -1,59 +1,88 @@
-// DOM elements and constants
-
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let completedTasks = JSON.parse(localStorage.getItem("completedTasks")) || [];
 let firstRun = true;
-
+const DOM = {};
 const keysArray = [
 	"addTaskForm",
 	"taskTitle",
 	"taskDesc",
 	"taskDateTime",
+	"completedOn",
 	"addTaskBtn",
 	"clearFormBtn",
 	"saveChangesBtn",
 	"cancelChangesBtn",
 	"taskList",
-	"cardBtns",
-	"editBtns",
-	"deleteBtns",
-]
-
-const DOMMap = {};
-
+	"completedTaskList",
+	"cardBtn",
+	"editBtn",
+	"deleteBtn",
+	"completeBtn",
+	"showCompletedTasksBtn",
+	"hideCompletedTasksBtn",
+];
 keysArray.forEach((key) => {
-	DOMMap[key] = document.querySelector(`.${key}`);
-})
-
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+	DOM[key] = document.querySelector(`.${key}`);
+});
 
 const displayTasks = (isNewTask) => {
-	DOMMap.taskList.innerHTML = "";
-	tasks.forEach((e, i) => {
+	DOM.taskList.innerHTML = "";
+	tasks.forEach((task, i) => {
 		const shouldFade = (isNewTask && i === tasks.length - 1) || firstRun;
-		DOMMap.taskList.innerHTML += `
-      <div class="col-sm-12 col-md-6 col-xl-3 taskCard">
-          <div class="card bg-transparent border-0 text-black ${shouldFade ? "animate-fade-in" : ""}" style="width: 20rem;">
-              <img src="images/notebg.png" class="card-img">
-              <div class="card-img-overlay">
-                <div class="cardBtns">
-                  	<i data-id="${i}" class="editBtns fa-solid fa-pen-to-square fa-lg"></i>
-                  	<i data-id="${i}" class="deleteBtns fa-solid fa-square-xmark fa-lg"></i>
-                </div>
-                  <div class="cardTitle"><h4>${e.taskTitle}</h4></div>
-                <div class="card-scroll-box">
-                    <p class="card-text ">${e.taskDesc}</p>
-                </div>
-                <p class="card-text taskDateTime">${e.taskDateTime}</p>
-              </div>
-          </div>
-      </div> 
-      `
+		DOM.taskList.innerHTML += `
+			<div class="col-sm-12 col-md-6 col-xl-3 taskCard">
+				<div class="card bg-transparent border-0 text-black ${shouldFade ? "animate-fade-in" : ""}" style="width: 20rem;">
+					<img src="images/notebg.png" class="card-img">
+					<div class="card-img-overlay">
+						<div class="cardBtn">
+							<i data-id="${i}" class="editBtn fa-solid fa-square-pen fa-lg" title="Edit Task"></i>
+							<i data-id="${i}" class="completeBtn fa-solid fa-square-check fa-lg" title="Mark as Complete"></i>
+							<i data-id="${i}" class="deleteBtn fa-solid fa-square-xmark fa-lg" title="Delete Task"></i>
+						</div>
+						<div class="cardTitle">
+							<h4>${task.taskTitle}</h4>
+						</div>
+						<div class="card-scroll-box">
+							<p class="card-text">${task.taskDesc}</p>
+						</div>
+						<p class="card-text taskDateTime">Due on ${task.taskDateTime}</p>
+					</div>
+				</div>
+			</div> 
+		`;
 	});
 	firstRun = false;
-}
+};
 
-const bindClickHandlers = () => {
-	DOMMap.addTaskBtn.addEventListener("click", () => {
-		if (!DOMMap.taskTitle.value || !DOMMap.taskDesc.value || !DOMMap.taskDateTime.value) {
+const displayCompletedTasks = () => {
+	DOM.completedTaskList.innerHTML = "";
+	completedTasks.forEach((task, i) => {
+		DOM.completedTaskList.innerHTML += `
+			<div class="col-sm-12 col-md-6 col-xl-3 taskCard">
+				<div class="card bg-transparent border-0 text-black" style="width: 20rem;">
+					<img src="images/notebg_comp.png" class="card-img">
+					<div class="card-img-overlay">
+						<div class="cardBtn">
+							<i data-id="${i}" class="deleteBtn complete fa-solid fa-square-xmark fa-lg" title="Delete Task"></i>
+						</div>
+						<div class="cardTitle">
+							<h4>${task.taskTitle}</h4>
+						</div>
+						<div class="card-scroll-box">
+							<p class="card-text">${task.taskDesc}</p>
+						</div>
+						<p class="card-text taskDateTime">Completed on ${task.completedOn}</p>
+					</div>
+				</div>
+			</div> 
+		`;
+	});
+};
+
+const clickHandlers = () => {
+	//Add task
+	DOM.addTaskBtn.addEventListener("click", () => {
+		if (!DOM.taskTitle.value || !DOM.taskDesc.value || !DOM.taskDateTime.value) {
 			Swal.fire({
 				icon: "error",
 				title: "Oops...",
@@ -62,71 +91,145 @@ const bindClickHandlers = () => {
 			});
 			return;
 		}
+		// Always returns error, even when date is in the future
+		// if (DOM.taskDateTime.value < new Date().toISOString().slice(0, 16)) {
+		// 	Swal.fire({
+		// 		icon: "error",
+		// 		title: "Oops...",
+		// 		text: "Your task due date must be in the future!",
+		// 		confirmButtonColor: "royalblue",
+		// 	});
+		// 	return;
+		// }
 		const task = {
-			taskTitle: DOMMap.taskTitle.value,
-			taskDesc: DOMMap.taskDesc.value,
-			taskDateTime: DOMMap.taskDateTime.value,
+			taskTitle: DOM.taskTitle.value,
+			taskDesc: DOM.taskDesc.value,
+			taskDateTime: DOM.taskDateTime.value,
+			completedOn: "",
 		};
 		tasks.push(task);
-		console.log(tasks);
 		localStorage.setItem("tasks", JSON.stringify(tasks));
 		displayTasks(true);
-		DOMMap.addTaskForm.reset();
+		DOM.addTaskForm.reset();
 	});
 
-	DOMMap.taskList.addEventListener("click", (event) => {
+	//Changes to task
+	DOM.taskList.addEventListener("click", (event) => {
 		const i = event.target.dataset.id;
 		const classList = event.target.classList;
+
 		//Delete task
-		if (classList.contains("deleteBtns")) {
+		if (classList.contains("deleteBtn")) {
 			Swal.fire({
 				title: "Are you sure you want to delete this task?",
 				showCancelButton: true,
 				confirmButtonText: "Yes",
-				confirmButtonColor: "darkorange",
+				confirmButtonColor: "black",
 				cancelButtonText: "Cancel",
 			}).then((result) => {
 				if (result.isConfirmed) {
 					tasks.splice(i, 1);
 					localStorage.setItem("tasks", JSON.stringify(tasks));
-					displayTasks();
+					displayTasks();	
 				}
 			});
-			//Edit task
-		} else if (classList.contains("editBtns")) {
-			DOMMap.addTaskBtn.style.display = "none";
-			DOMMap.saveChangesBtn.style.display = "inline";
-			DOMMap.cancelChangesBtn.style.display = "inline";
-			DOMMap.taskTitle.value = tasks[i].taskTitle;
-			DOMMap.taskDesc.value = tasks[i].taskDesc;
-			DOMMap.taskDateTime.value = tasks[i].taskDateTime;
-			DOMMap.saveChangesBtn.addEventListener("click", () => {
-				tasks[i].taskTitle = DOMMap.taskTitle.value;
-				tasks[i].taskDesc = DOMMap.taskDesc.value;
-				tasks[i].taskDateTime = DOMMap.taskDateTime.value;
+		}
+
+		//Edit task
+		if (classList.contains("editBtn")) {
+			DOM.addTaskBtn.style.display = "none";
+			DOM.saveChangesBtn.style.display = "inline";
+			DOM.cancelChangesBtn.style.display = "inline";
+			DOM.taskTitle.value = tasks[i].taskTitle;
+			DOM.taskDesc.value = tasks[i].taskDesc;
+			DOM.taskDateTime.value = tasks[i].taskDateTime;
+			DOM.saveChangesBtn.addEventListener("click", () => {
+				if (!DOM.taskTitle.value || !DOM.taskDesc.value || !DOM.taskDateTime.value) {
+					Swal.fire({
+						icon: "error",
+						title: "Oops...",
+						text: "Please fill out all fields!",
+						confirmButtonColor: "royalblue",
+					});
+					return;
+				}
+				tasks[i].taskTitle = DOM.taskTitle.value;
+				tasks[i].taskDesc = DOM.taskDesc.value;
+				tasks[i].taskDateTime = DOM.taskDateTime.value;
 				localStorage.setItem("tasks", JSON.stringify(tasks));
 				displayTasks();
-				DOMMap.addTaskForm.reset();
-				DOMMap.addTaskBtn.style.display = "inline";
-				DOMMap.saveChangesBtn.style.display = "none";
-				DOMMap.cancelChangesBtn.style.display = "none";
+				DOM.addTaskForm.reset();
+				DOM.addTaskBtn.style.display = "inline";
+				DOM.saveChangesBtn.style.display = "none";
+				DOM.cancelChangesBtn.style.display = "none";
 			});
-			//Cancel changes
-			DOMMap.cancelChangesBtn.addEventListener("click", () => {
-				DOMMap.addTaskForm.reset();
-				DOMMap.addTaskBtn.style.display = "inline";
-				DOMMap.saveChangesBtn.style.display = "none";
-				DOMMap.cancelChangesBtn.style.display = "none";
-			});
+		};
+
+		//Mark task as complete
+		if (classList.contains("completeBtn")) {
+			tasks[i].completedOn = new Date().toLocaleString();
+			completedTasks.push(tasks[i]);
+			tasks.splice(i, 1);
+			localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
+			localStorage.setItem("tasks", JSON.stringify(tasks));
+			DOM.showCompletedTasksBtn.style.display = "inline";
+			displayTasks();
 		}
+
+		//Cancel changes
+		DOM.cancelChangesBtn.addEventListener("click", () => {
+			DOM.addTaskForm.reset();
+			DOM.addTaskBtn.style.display = "inline";
+			DOM.saveChangesBtn.style.display = "none";
+			DOM.cancelChangesBtn.style.display = "none";
+		});
 	});
 
-	DOMMap.clearFormBtn.addEventListener("click", () => {
-		DOMMap.addTaskForm.reset();
+	DOM.clearFormBtn.addEventListener("click", () => {
+		DOM.addTaskForm.reset();
 	});
+
+	DOM.showCompletedTasksBtn.addEventListener("click", () => {
+		displayCompletedTasks();
+		DOM.completedTaskList.style.display = "inline";
+		DOM.showCompletedTasksBtn.style.display = "none";
+		DOM.hideCompletedTasksBtn.style.display = "inline";
+	});
+	DOM.hideCompletedTasksBtn.addEventListener("click", () => {
+			DOM.completedTaskList.style.display = "none";
+			DOM.showCompletedTasksBtn.style.display = "inline";
+			DOM.hideCompletedTasksBtn.style.display = "none";
+	});
+
+    //Delete task from completed tasklist
+	DOM.completedTaskList.addEventListener("click", (event) => {
+		i = event.target.dataset.id;
+		Swal.fire({
+			title: "Are you sure you want to delete this task?",
+			showCancelButton: true,
+			confirmButtonText: "Yes",
+			confirmButtonColor: "black",
+			cancelButtonText: "Cancel",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				completedTasks.splice(i, 1);
+				localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
+				displayCompletedTasks();
+			}
+		//Disply Show and hide completed tasklist buttons
+			if (completedTasks.length > 0) {
+				DOM.showCompletedTasksBtn.style.display = "inline";
+				//DOM.showCompletedTasksBtn.focus();
+			} else { 
+				DOM.showCompletedTasksBtn.style.display = "none";
+				DOM.hideCompletedTasksBtn.style.display = "none";
+			};
+			
+		});
+	});	
 };
 
-// Other Components
+// Run on page load
 
 flatpickr(".taskDateTime", {
 	enableTime: true,
@@ -134,10 +237,6 @@ flatpickr(".taskDateTime", {
 	time_24hr: true,
 });
 
-
-// Run on page load
-
-//Attntion: The function below is not working properly.
-bindClickHandlers();
+clickHandlers();
 
 displayTasks();
