0b9db1369173ce24b6654bbd972fd0a5dfe4fbc5
BenHaran <ben@doorloop.com>
2024-05-07 01:05:16 +0300

    Move to working with/saving unix timestamps Clean code via small functions (hide/show/formatDate/toUnixTS) Fix Completed List buttons behavior

--- JB_Project_01/script.js
+++ JB_Project_01/script.js
@@ -25,19 +25,43 @@ keysArray.forEach((key) => {
 	DOM[key] = document.querySelector(`.${key}`);
 });
 
-const displayTasks = (isNewTask) => {
-	DOM.taskList.innerHTML = "";
-	tasks.forEach((task, i) => {
-		const shouldFade = (isNewTask && i === tasks.length - 1) || firstRun;
-		DOM.taskList.innerHTML += `
-			<div class="col-sm-12 col-md-6 col-xl-3 taskCard">
-				<div class="card bg-transparent border-0 text-black ${shouldFade ? "animate-fade-in" : ""}" style="width: 20rem;">
-					<img src="images/notebg.png" class="card-img">
+const showElements = (...elements) => {
+	elements.forEach(element => {
+		element.style.display = "inline";
+	});
+}
+const hideElements = (...elements) => {
+	elements.forEach(element => {
+		element.style.display = "none";
+	})
+}
+
+if (completedTasks.length > 0) {
+	showElements(DOM.showCompletedTasksBtn);
+}
+
+const formatDateTime = (unixTimestamp) => {
+	const date = new Date(unixTimestamp);
+	const day = date.getDate();
+	const month = date.getMonth() + 1;
+	const year = date.getFullYear();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+	return `${day.toString().padStart(2, "0")}/${month.toString().padStart(2, "0")}/${year} ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+}
+
+const getTaskHTML = (task, i, shouldFade, isCompleted) => {
+	return `
+		<div class="col-sm-12 col-md-6 col-xl-3 taskCard">
+			<div class="card bg-transparent border-0 text-black ${shouldFade ? "animate-fade-in" : ""}" style="width: 20rem;">
+				<img src="images/notebg.png" class="card-img">
 					<div class="card-img-overlay">
 						<div class="cardBtn">
-							<i data-id="${i}" class="editBtn fa-solid fa-square-pen fa-lg" title="Edit Task"></i>
-							<i data-id="${i}" class="completeBtn fa-solid fa-square-check fa-lg" title="Mark as Complete"></i>
-							<i data-id="${i}" class="deleteBtn fa-solid fa-square-xmark fa-lg" title="Delete Task"></i>
+							${isCompleted ? "" : `
+								<i data-id="${i}" class="editBtn fa-solid fa-square-pen fa-lg" title="Edit Task"></i>
+								<i data-id="${i}" class="completeBtn fa-solid fa-square-check fa-lg" title="Mark as Complete"></i>
+							`}
+							<i data-id="${i}" class="${isCompleted ? 'complete' : ''} deleteBtn fa-solid fa-square-xmark fa-lg" title="Delete Task"></i>
 						</div>
 						<div class="cardTitle">
 							<h4>${task.taskTitle}</h4>
@@ -45,11 +69,20 @@ const displayTasks = (isNewTask) => {
 						<div class="card-scroll-box">
 							<p class="card-text">${task.taskDesc}</p>
 						</div>
-						<p class="card-text taskDateTime">Due on ${task.taskDateTime}</p>
+						<p class="card-text taskDateTime">${isCompleted ? 'Completed' : 'Due'} on ${formatDateTime(isCompleted ? task.completedOn : task.taskDateTime)}</p>
 					</div>
-				</div>
-			</div> 
-		`;
+			</div>
+		</div>
+	`
+}
+
+const toProperUnix = (date) => (+date) * 1000;
+
+const displayTasks = (isNewTask) => {
+	DOM.taskList.innerHTML = "";
+	tasks.forEach((task, i) => {
+		const shouldFade = (isNewTask && i === tasks.length - 1) || firstRun;
+		DOM.taskList.innerHTML += getTaskHTML(task, i, shouldFade, false);
 	});
 	firstRun = false;
 };
@@ -57,25 +90,7 @@ const displayTasks = (isNewTask) => {
 const displayCompletedTasks = () => {
 	DOM.completedTaskList.innerHTML = "";
 	completedTasks.forEach((task, i) => {
-		DOM.completedTaskList.innerHTML += `
-			<div class="col-sm-12 col-md-6 col-xl-3 taskCard">
-				<div class="card bg-transparent border-0 text-black" style="width: 20rem;">
-					<img src="images/notebg_comp.png" class="card-img">
-					<div class="card-img-overlay">
-						<div class="cardBtn">
-							<i data-id="${i}" class="deleteBtn complete fa-solid fa-square-xmark fa-lg" title="Delete Task"></i>
-						</div>
-						<div class="cardTitle">
-							<h4>${task.taskTitle}</h4>
-						</div>
-						<div class="card-scroll-box">
-							<p class="card-text">${task.taskDesc}</p>
-						</div>
-						<p class="card-text taskDateTime">Completed on ${task.completedOn}</p>
-					</div>
-				</div>
-			</div> 
-		`;
+		DOM.completedTaskList.innerHTML += getTaskHTML(task, i, false, true);
 	});
 };
 
@@ -91,8 +106,7 @@ const clickHandlers = () => {
 			});
 			return;
 		}
-		// Always returns error, even when date is in the future
-		// if (DOM.taskDateTime.value < new Date().toISOString().slice(0, 16)) {
+		// if (toProperUnix(DOM.taskDateTime.value) < Date.now()) {
 		// 	Swal.fire({
 		// 		icon: "error",
 		// 		title: "Oops...",
@@ -104,8 +118,7 @@ const clickHandlers = () => {
 		const task = {
 			taskTitle: DOM.taskTitle.value,
 			taskDesc: DOM.taskDesc.value,
-			taskDateTime: DOM.taskDateTime.value,
-			completedOn: "",
+			taskDateTime: toProperUnix(DOM.taskDateTime.value),
 		};
 		tasks.push(task);
 		localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -137,12 +150,11 @@ const clickHandlers = () => {
 
 		//Edit task
 		if (classList.contains("editBtn")) {
-			DOM.addTaskBtn.style.display = "none";
-			DOM.saveChangesBtn.style.display = "inline";
-			DOM.cancelChangesBtn.style.display = "inline";
+			hideElements(DOM.addTaskBtn);
+			showElements(DOM.saveChangesBtn, DOM.cancelChangesBtn);
 			DOM.taskTitle.value = tasks[i].taskTitle;
 			DOM.taskDesc.value = tasks[i].taskDesc;
-			DOM.taskDateTime.value = tasks[i].taskDateTime;
+			picker.setDate(new Date(tasks[i].taskDateTime))
 			DOM.saveChangesBtn.addEventListener("click", () => {
 				if (!DOM.taskTitle.value || !DOM.taskDesc.value || !DOM.taskDateTime.value) {
 					Swal.fire({
@@ -155,33 +167,31 @@ const clickHandlers = () => {
 				}
 				tasks[i].taskTitle = DOM.taskTitle.value;
 				tasks[i].taskDesc = DOM.taskDesc.value;
-				tasks[i].taskDateTime = DOM.taskDateTime.value;
+				tasks[i].taskDateTime = toProperUnix(DOM.taskDateTime.value);
 				localStorage.setItem("tasks", JSON.stringify(tasks));
 				displayTasks();
 				DOM.addTaskForm.reset();
-				DOM.addTaskBtn.style.display = "inline";
-				DOM.saveChangesBtn.style.display = "none";
-				DOM.cancelChangesBtn.style.display = "none";
+				showElements(DOM.addTaskBtn);
+				hideElements(DOM.saveChangesBtn, DOM.cancelChangesBtn);
 			});
 		};
 
 		//Mark task as complete
 		if (classList.contains("completeBtn")) {
-			tasks[i].completedOn = new Date().toLocaleString();
+			tasks[i].completedOn = Date.now();
 			completedTasks.push(tasks[i]);
 			tasks.splice(i, 1);
 			localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
 			localStorage.setItem("tasks", JSON.stringify(tasks));
-			DOM.showCompletedTasksBtn.style.display = "inline";
+			showElements(DOM.showCompletedTasksBtn);
 			displayTasks();
 		}
 
 		//Cancel changes
 		DOM.cancelChangesBtn.addEventListener("click", () => {
 			DOM.addTaskForm.reset();
-			DOM.addTaskBtn.style.display = "inline";
-			DOM.saveChangesBtn.style.display = "none";
-			DOM.cancelChangesBtn.style.display = "none";
+			showElements(DOM.addTaskBtn);
+			hideElements(DOM.saveChangesBtn, DOM.cancelChangesBtn);
 		});
 	});
 
@@ -191,17 +201,15 @@ const clickHandlers = () => {
 
 	DOM.showCompletedTasksBtn.addEventListener("click", () => {
 		displayCompletedTasks();
-		DOM.completedTaskList.style.display = "inline";
-		DOM.showCompletedTasksBtn.style.display = "none";
-		DOM.hideCompletedTasksBtn.style.display = "inline";
+		showElements(DOM.completedTaskList, DOM.hideCompletedTasksBtn);
+		hideElements(DOM.showCompletedTasksBtn);
 	});
 	DOM.hideCompletedTasksBtn.addEventListener("click", () => {
-			DOM.completedTaskList.style.display = "none";
-			DOM.showCompletedTasksBtn.style.display = "inline";
-			DOM.hideCompletedTasksBtn.style.display = "none";
+			showElements(DOM.showCompletedTasksBtn);
+			hideElements(DOM.completedTaskList, DOM.hideCompletedTasksBtn);
 	});
 
-    //Delete task from completed tasklist
+	//Delete task from completed tasklist
 	DOM.completedTaskList.addEventListener("click", (event) => {
 		i = event.target.dataset.id;
 		Swal.fire({
@@ -215,28 +223,32 @@ const clickHandlers = () => {
 				completedTasks.splice(i, 1);
 				localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
 				displayCompletedTasks();
+				if (completedTasks.length === 0) {
+					hideElements(DOM.showCompletedTasksBtn, DOM.hideCompletedTasksBtn);
+				};
 			}
 		//Disply Show and hide completed tasklist buttons
-			if (completedTasks.length > 0) {
-				DOM.showCompletedTasksBtn.style.display = "inline";
-				//DOM.showCompletedTasksBtn.focus();
-			} else { 
-				DOM.showCompletedTasksBtn.style.display = "none";
-				DOM.hideCompletedTasksBtn.style.display = "none";
-			};
-			
+
 		});
 	});	
 };
 
 // Run on page load
 
-flatpickr(".taskDateTime", {
+const picker = flatpickr(".taskDateTime", {
 	enableTime: true,
-	dateFormat: "d/m/Y H:i",
+	altInput: true,
+	altFormat: "d/m/Y H:i",
+	dateFormat: "U",
 	time_24hr: true,
+	minDate: Date.now()
 });
 
 clickHandlers();
 
 displayTasks();
+
+
+// Todo: Fix the CSS (new date field isn't wide enough, completed list is always one column)
+// Todo: Reuse code for edit and delete and add buttons AKA avoid code duplication
+// Todo: Duplicate Task
